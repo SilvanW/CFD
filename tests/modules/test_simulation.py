fdd01362d0cec9cb7f8a8e3a5fbe7dd3124b2679
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-11-02 17:07:03 +0100

    add simulation functionality and central difference tests

---
+++ tests/modules/test_simulation.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from app.modules.configuration import AppConfig, read_config
+from app.modules.domain import (
+    Layer,
+    generate_simulation_grid,
+    get_simulation_grid_value,
+    set_simulation_grid_value,
+)
+from app.modules.simulation import Direction, central_difference
+
+
+@pytest.fixture
+def test_config() -> AppConfig:
+    config = read_config(Path("tests/test_config.json"))
+    config.domain.grid_points_x = 3
+    config.domain.grid_points_y = 3
+
+    return config
+
+
+@pytest.fixture
+def simulation_grid(test_config: AppConfig) -> np.ndarray:
+    return generate_simulation_grid(test_config.domain)
+
+
+def test_central_difference_u_x(simulation_grid):
+    set_simulation_grid_value(simulation_grid, Layer.VELOCITY_X, 1, 2, 2)
+    set_simulation_grid_value(simulation_grid, Layer.VELOCITY_X, 3, 2, 4)
+
+    differenced_grid = central_difference(
+        simulation_grid, Layer.VELOCITY_X, 2, 2, 1, Direction.X
+    )
+    assert get_simulation_grid_value(differenced_grid, Layer.VELOCITY_X, 2, 2) == 1
+
+
+def test_central_difference_u_y(simulation_grid):
+    set_simulation_grid_value(simulation_grid, Layer.VELOCITY_X, 2, 1, 2)
+    set_simulation_grid_value(simulation_grid, Layer.VELOCITY_X, 2, 3, 4)
+
+    differenced_grid = central_difference(
+        simulation_grid, Layer.VELOCITY_X, 2, 2, 1, Direction.Y
+    )
+
+    assert get_simulation_grid_value(differenced_grid, Layer.VELOCITY_X, 2, 2) == 1
+
+
+def test_central_difference_v_x(simulation_grid):
+    set_simulation_grid_value(simulation_grid, Layer.VELOCITY_Y, 1, 2, 2)
+    set_simulation_grid_value(simulation_grid, Layer.VELOCITY_Y, 3, 2, 4)
+
+    differenced_grid = central_difference(
+        simulation_grid, Layer.VELOCITY_Y, 2, 2, 1, Direction.X
+    )
+
+    assert get_simulation_grid_value(differenced_grid, Layer.VELOCITY_Y, 2, 2) == 1
+
+
+def test_central_difference_v_y(simulation_grid):
+    set_simulation_grid_value(simulation_grid, Layer.VELOCITY_Y, 2, 1, 2)
+    set_simulation_grid_value(simulation_grid, Layer.VELOCITY_Y, 2, 3, 4)
+
+    differenced_grid = central_difference(
+        simulation_grid, Layer.VELOCITY_Y, 2, 2, 1, Direction.Y
+    )
+
+    assert get_simulation_grid_value(differenced_grid, Layer.VELOCITY_Y, 2, 2) == 1
