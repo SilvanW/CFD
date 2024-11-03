04a9ff0754d5c0006bc5e31669497dec2f036e83
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-11-03 14:54:56 +0100

    add option to show boundary box in plot

--- app/modules/visualisation.py
+++ app/modules/visualisation.py
@@ -2,10 +2,10 @@
 This Module is responsible for Domain Visualisation
 """
 
+import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import figure
-import matplotlib.pyplot as plt
-
+from matplotlib.lines import Line2D
 from modules.domain import Layer
 
 
@@ -23,11 +23,14 @@ def plot_pressure_heatmap(simulation_grid: np.ndarray) -> figure:
     plt.title("Druckfeld")
 
 
-def plot_velocity_quiver_plot(simulation_grid: np.ndarray) -> figure:
+def plot_velocity_quiver_plot(
+    simulation_grid: np.ndarray, show_boundary_box: bool = False
+) -> figure:
     """Plot the Velocity Field from the Simulation Grid as Quiver Plot
 
     Args:
         simulation_grid (np.ndarray): Simulation Grid
+        show_boundary_box (bool, optional): Show Boundary Box. Defaults to False.
 
     Returns:
         figure: plt.quiver Visualisation
@@ -39,6 +42,23 @@ def plot_velocity_quiver_plot(simulation_grid: np.ndarray) -> figure:
     velocity_x = velocity_x / np.sqrt(velocity_x**2 + velocity_y**2)
     velocity_y = velocity_y / np.sqrt(velocity_x**2 + velocity_y**2)
 
-    plt.quiver(velocity_x, velocity_y, scale=25)
+    plt.quiver(velocity_x, velocity_y, headwidth=2)
 
     plt.title("Geschwindigkeitsfeld")
+
+    if show_boundary_box:
+        y_start = 0.5
+        y_end = simulation_grid.shape[1] - 1.5
+
+        x_start = 0.5
+        x_end = simulation_grid.shape[2] - 1.5
+
+        top_line = Line2D([x_start, x_end], [y_end, y_end], color="blue")
+        left_line = Line2D([x_start, x_start], [y_start, y_end], color="blue")
+        bottom_line = Line2D([x_start, x_end], [y_start, y_start], color="blue")
+        right_line = Line2D([x_end, x_end], [y_start, y_end], color="blue")
+
+        plt.gca().add_line(top_line)
+        plt.gca().add_line(left_line)
+        plt.gca().add_line(bottom_line)
+        plt.gca().add_line(right_line)
