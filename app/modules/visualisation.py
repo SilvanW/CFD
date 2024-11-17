344d47dae48ecbebb6f7852ba0e576b99c98b4f4
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-11-17 17:09:38 +0100

    add boundary box to pressure map

--- app/modules/visualisation.py
+++ app/modules/visualisation.py
@@ -9,18 +9,40 @@ from matplotlib.lines import Line2D
 from modules.domain import Layer
 
 
-def plot_pressure_heatmap(simulation_grid: np.ndarray) -> figure:
+def plot_pressure_heatmap(
+    simulation_grid: np.ndarray, show_boundary_box: bool = False
+) -> figure:
     """Plot the Pressure Field from the Simulation Grid as a Heatmap
 
     Args:
         simulation_grid (np.ndarray): Simulation Grid
+        show_boundary_box (bool, optional): Show Boundary Box. Defaults to False.
 
     Returns:
         figure: plt.imshow Visualisation
     """
-    plt.imshow(simulation_grid[Layer.PRESSURE.value], origin="lower")
+    imshow = plt.imshow(simulation_grid[Layer.PRESSURE.value], origin="lower")
+
+    plt.colorbar(imshow)
 
     plt.title("Druckfeld")
+
+    if show_boundary_box:
+        y_start = 0.5
+        y_end = simulation_grid.shape[1] - 1.5
+
+        x_start = 0.5
+        x_end = simulation_grid.shape[2] - 1.5
+
+        top_line = Line2D([x_start, x_end], [y_end, y_end], color="gray")
+        left_line = Line2D([x_start, x_start], [y_start, y_end], color="gray")
+        bottom_line = Line2D([x_start, x_end], [y_start, y_start], color="gray")
+        right_line = Line2D([x_end, x_end], [y_start, y_end], color="gray")
+
+        plt.gca().add_line(top_line)
+        plt.gca().add_line(left_line)
+        plt.gca().add_line(bottom_line)
+        plt.gca().add_line(right_line)
 
 
 def plot_velocity_quiver_plot(
@@ -57,10 +79,10 @@ def plot_velocity_quiver_plot(
         x_start = 0.5
         x_end = simulation_grid.shape[2] - 1.5
 
-        top_line = Line2D([x_start, x_end], [y_end, y_end], color="blue")
-        left_line = Line2D([x_start, x_start], [y_start, y_end], color="blue")
-        bottom_line = Line2D([x_start, x_end], [y_start, y_start], color="blue")
-        right_line = Line2D([x_end, x_end], [y_start, y_end], color="blue")
+        top_line = Line2D([x_start, x_end], [y_end, y_end], color="gray")
+        left_line = Line2D([x_start, x_start], [y_start, y_end], color="gray")
+        bottom_line = Line2D([x_start, x_end], [y_start, y_start], color="gray")
+        right_line = Line2D([x_end, x_end], [y_start, y_end], color="gray")
 
         plt.gca().add_line(top_line)
         plt.gca().add_line(left_line)
