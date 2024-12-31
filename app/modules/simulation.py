3de8712acbe8e8421615cb6a826b9bcd5b320379
SilvanW <silvan.wiedmer@bluewin.ch>
2024-12-31 18:06:55 +0100

    remove unused code

--- app/modules/simulation.py
+++ app/modules/simulation.py
@@ -117,19 +117,6 @@ def get_intermediate_velocity(
     delta_t: float,
     grid_cell_size: float,
 ) -> float:
-    """
-    return (
-        (
-            -central_difference_value(
-                simulation_grid, layer, x_coord, y_coord, direction, grid_cell_size
-            )
-            * get_simulation_grid_value(simulation_grid, layer, x_coord, y_coord)
-            + dynamic_viscosity
-            * laplace_operator_value(simulation_grid, layer, x_coord, y_coord, 1)
-        )
-        * delta_t
-    ) + get_simulation_grid_value(simulation_grid, layer, x_coord, y_coord)
-    """
     if layer == Layer.VELOCITY_X:
         return get_simulation_grid_value(
             simulation_grid, layer, x_coord, y_coord
@@ -238,8 +225,35 @@ def get_new_pressure_value(
     grid_cell_size: int,
 ) -> float:
     return (
-        (
-            right_hand_side_value(
+        1
+        / 4
+        * (
+            get_simulation_grid_value(
+                simulation_grid,
+                Layer.PRESSURE,
+                target_x_coordinate + 1,
+                target_y_coordinate,
+            )
+            + get_simulation_grid_value(
+                simulation_grid,
+                Layer.PRESSURE,
+                target_x_coordinate - 1,
+                target_y_coordinate,
+            )
+            + get_simulation_grid_value(
+                simulation_grid,
+                Layer.PRESSURE,
+                target_x_coordinate,
+                target_y_coordinate + 1,
+            )
+            + get_simulation_grid_value(
+                simulation_grid,
+                Layer.PRESSURE,
+                target_x_coordinate,
+                target_y_coordinate - 1,
+            )
+            - grid_cell_size**2
+            * right_hand_side_value(
                 simulation_grid,
                 density,
                 delta_t,
@@ -247,50 +261,30 @@ def get_new_pressure_value(
                 target_y_coordinate,
                 grid_cell_size,
             )
-            * grid_cell_size**2
-            - get_simulation_grid_value(
-                simulation_grid,
-                Layer.PRESSURE,
-                target_x_coordinate + 1,
-                target_y_coordinate,
-            )
-            - get_simulation_grid_value(
-                simulation_grid,
-                Layer.PRESSURE,
-                target_x_coordinate - 1,
-                target_y_coordinate,
-            )
-            - get_simulation_grid_value(
-                simulation_grid,
-                Layer.PRESSURE,
-                target_x_coordinate,
-                target_y_coordinate + 1,
-            )
-            - get_simulation_grid_value(
-                simulation_grid,
-                Layer.PRESSURE,
-                target_x_coordinate,
-                target_y_coordinate - 1,
-            )
         )
-        * 1
-        / -4
     )
 
 
 def calculate_pressure_residual(
     simulation_grid: np.ndarray, density: float, delta_t: int, grid_cell_size: float
 ) -> float:
-    residual = 0.0
+    residual = np.zeros_like(simulation_grid)
     for x_coord in range(1, simulation_grid.shape[2] - 1):
         for y_coord in range(1, simulation_grid.shape[1] - 1):
-            residual += laplace_operator_value(
-                simulation_grid, Layer.PRESSURE, x_coord, y_coord, grid_cell_size
-            ) - right_hand_side_value(
-                simulation_grid, density, delta_t, x_coord, y_coord, grid_cell_size
+            set_simulation_grid_value(
+                residual,
+                Layer.PRESSURE,
+                x_coord,
+                y_coord,
+                laplace_operator_value(
+                    simulation_grid, Layer.PRESSURE, x_coord, y_coord, grid_cell_size
+                )
+                - right_hand_side_value(
+                    simulation_grid, density, delta_t, x_coord, y_coord, grid_cell_size
+                ),
             )
 
-    return residual
+    return np.sqrt(np.sum(residual[Layer.PRESSURE.value] ** 2))
 
 
 def update_pressure_grid(
@@ -307,7 +301,7 @@ def update_pressure_grid(
         for x_coord in range(1, simulation_grid.shape[2] - 1):
             for y_coord in range(1, simulation_grid.shape[1] - 1):
                 set_simulation_grid_value(
-                    sim_grid_next,
+                    sim_grid_next,  # store pressure value calculated form original grid in temp
                     Layer.PRESSURE,
                     x_coord,
                     y_coord,
@@ -323,13 +317,13 @@ def update_pressure_grid(
 
         enforce_pressure_boundary_condition(sim_grid_next, domain_config)
 
-        norm_pn = np.linalg.norm(sim_grid_next[Layer.PRESSURE.value])
-        norm_po = np.linalg.norm(simulation_grid[Layer.PRESSURE.value])
-        pressure_residual = np.abs(norm_pn - norm_po)
-
         simulation_grid[Layer.PRESSURE.value] = sim_grid_next[
             Layer.PRESSURE.value
         ].copy()
+
+        pressure_residual = calculate_pressure_residual(
+            simulation_grid, density, delta_t, grid_cell_size
+        )
 
         if pressure_residual <= 1e-5:
             print("Pressure residual Small enough")
@@ -364,3 +358,37 @@ def calculate_velocity_residual(
             ) ** 2
 
     return np.sqrt(residual)
+
+
+def velocity_continuity(
+    simulation_grid: np.ndarray, domain_config: DomainConfig
+) -> np.ndarray:
+    divergence = np.zeros_like(simulation_grid)
+
+    # Ignore outer cells next to boundary
+    for x_coord in range(2, simulation_grid.shape[2] - 2):
+        for y_coord in range(2, simulation_grid.shape[1] - 2):
+            set_simulation_grid_value(
+                divergence,
+                Layer.PRESSURE,
+                x_coord,
+                y_coord,
+                central_difference_value(
+                    simulation_grid,
+                    Layer.VELOCITY_X,
+                    x_coord,
+                    y_coord,
+                    Direction.X,
+                    domain_config.grid_cell_size,
+                )
+                + central_difference_value(
+                    simulation_grid,
+                    Layer.VELOCITY_Y,
+                    x_coord,
+                    y_coord,
+                    Direction.Y,
+                    domain_config.grid_cell_size,
+                ),
+            )
+
+    return divergence
